5e4d71808450d835253d794af1ca0389a18f3ebe
Harshit Agrawal <harshitagrawal.iitr@gmail.com>
2018-06-06 22:30:53 +0530

    Worked on the Logistic model

--- js/index.js
+++ js/index.js
@@ -5,16 +5,23 @@ $(document).ready(function(){
   setup();
 })
 
+var line = d3.line()
+    .x(function(d) { return (d.x); })
+    .y(function(d) { return (d.y); });
+
 /***************************************************************************/
 // Setup function
 
 function setup(){
   obj = new LogisiticModel();
   obj.createSliders();
+
   obj.parse();
   obj.renderEquation(); // Call after calling parse()
   MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
   obj.findAllRoots();
+
+  // obj.createGraph();
 
   update();
   MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
@@ -28,4 +35,6 @@ function update(){
 
   obj.updateSliders();
   obj.draw();
+
+  // obj.updateGraph();
 }
--- js/logisticModel.js
+++ js/logisticModel.js
@@ -4,7 +4,7 @@
 function LogisiticModel(){
   this.exp = 'r*N*(1-N/K)';
   // this.exp_latex = "\\( \\frac{dN}{dt} = r*N*(1- \\frac{N}{K} ) \\)";
-  this.heading = 'Logisitc Model';
+  this.heading = 'Logisitic Model';
 
   this.r = { min: 0.1, max: 5, step: 0.1, value: 1 };
   this.K = { min: 0.1, max: 5, step: 0.1, value: 1 };
@@ -196,6 +196,122 @@ LogisiticModel.prototype.updateSliders = function(){
 
 /******************************************************************************/
 
+// LogisiticModel.prototype.createGraph = function(){
+//   this.graph_1 = new phasePlot('chart_1');
+//   this.graph_1.create('Population →', 'Rate of change in population →');
+// }
+//
+// LogisiticModel.prototype.updateGraph = function(){
+//   this.graph_1.update_path(this.N, this.dN_by_dt_list); // x_data, y_data
+//   this.graph_1.update_roots(this.roots, this.roots_stability);
+// }
+
+/******************************************************************************/
+
+// function phasePlot(div_id, roots_count){
+//   this.margin = 25;
+//   this.div_id = div_id;
+//
+//   this.create = function(x_label, y_label){
+//     this.div = d3.select('#'+this.div_id);
+//     this.width = parseInt( this.div.style('width') );
+//     this.height = 0.75*this.width;
+//     this.svg = this.div.append('svg').attrs({ width: this.width, height: this.height });
+//
+//     this.origin = this.svg.append('g');
+//     this.x_axis = this.svg.append('g');
+//     this.y_axis = this.svg.append('g');
+//     this.path = this.svg.append('path').attrs({ class: 'blue_path' });
+//
+//     this.x_label = this.svg.append('text').text(x_label);
+//     this.y_label = this.svg.append('text').text(y_label);
+//
+//     var limit_x_left = this.margin, limit_x_right = this.width - 2*this.margin;
+//     var limit_y_top = this.margin, limit_y_bottom = this.height - 2*this.margin;
+//     this.x_scale = d3.scaleLinear().range([limit_x_left, limit_x_right]);
+//     this.y_scale = d3.scaleLinear().range([limit_y_bottom, limit_y_top]);
+//   }
+//
+//   this.update_path = function(x_data, y_data){
+//     this.x_data = x_data;
+//     this.y_data = y_data;
+//
+//     this.x_scale.domain(d3.extent(this.x_data));
+//     this.y_scale.domain(d3.extent(this.y_data));
+//
+//     this.origin.attrs({ transform: 'translate(' +this.x_scale(0)+ ',' +this.y_scale(0)+ ')' })
+//
+//     this.x_axis.attrs({ transform: 'translate(' +0+ ',' +this.y_scale(0)+ ')' }).call( d3.axisBottom(this.x_scale) );
+//     this.y_axis.attrs({ transform: 'translate(' +this.x_scale(0)+ ',' +0+ ')' }).call( d3.axisLeft(this.y_scale) );
+//     this.x_axis.select('.tick').styles({ 'display': 'none' });
+//
+//     this.path_data = this.x_data.map((d,i) => {
+//       return { x: this.x_scale(this.x_data[i]), y: this.y_scale(this.y_data[i]) }
+//     })
+//     this.path.attrs({ d: line( this.path_data ) });
+//   }
+//
+//   this.update_roots = function(roots, stability){
+//     this.roots = [];
+//     this.roots = roots.map((d,i) => { return { value: roots[i], stability: stability[i] } });
+//     d3.selectAll('.roots').remove();
+//     this.roots.forEach(d => {
+//       d.node = this.svg.append('circle').attrs({ cx: this.x_scale(d.value), cy: this.y_scale(0), r: 3, class: 'roots' });
+//       if(d.stability == 'stable'){ d.node.classed('stable', true); } else { d.node.classed('unstable', true); }
+//     })
+//   }
+//
+//   this.simulate_node = function(){
+//
+//   }
+//
+// }
+
+/******************************************************************************/
+
+// LogisiticModel.prototype.createGraph_1 = function(){
+//   this.graph_1 = {};
+//   var graph_1 = this.graph_1;
+//   var margin = 25;
+//
+//   graph_1.div = d3.select('#chart_1');
+//   graph_1.width = parseInt( graph_1.div.style('width') );
+//   graph_1.height = 0.75*graph_1.width;
+//   graph_1.svg = graph_1.div.append('svg').attrs({ width: graph_1.width, height: graph_1.height });
+//
+//   graph_1.origin = graph_1.svg.append('g');
+//   graph_1.x_axis = graph_1.svg.append('g');
+//   graph_1.y_axis = graph_1.svg.append('g');
+//   graph_1.path = graph_1.svg.append('path').attrs({ class: 'blue_path' });
+//
+//   var limit_x_left = margin, limit_x_right = graph_1.width - 2*margin;
+//   var limit_y_top = margin, limit_y_bottom = graph_1.height - 2*margin;
+//   graph_1.x_scale = d3.scaleLinear().range([limit_x_left, limit_x_right]);
+//   graph_1.y_scale = d3.scaleLinear().range([limit_y_bottom, limit_y_top]);
+// }
+//
+// LogisiticModel.prototype.updateGraph_1 = function(){
+//   var graph_1 = this.graph_1;
+//   graph_1.x_data = this.N;
+//   graph_1.x_scale.domain(d3.extent(graph_1.x_data));
+//
+//   graph_1.y_data = this.dN_by_dt_list;
+//   graph_1.y_scale.domain(d3.extent(graph_1.y_data));
+//
+//   graph_1.origin.attrs({ transform: 'translate(' +graph_1.x_scale(0)+ ',' +graph_1.y_scale(0)+ ')' })
+//
+//   graph_1.x_axis.attrs({ transform: 'translate(' +0+ ',' +graph_1.y_scale(0)+ ')' }).call( d3.axisBottom(graph_1.x_scale) );
+//   graph_1.y_axis.attrs({ transform: 'translate(' +graph_1.x_scale(0)+ ',' +0+ ')' }).call( d3.axisLeft(graph_1.y_scale) );
+//   graph_1.x_axis.select('.tick').styles({ 'display': 'none' });
+//
+//   graph_1.path_data = graph_1.x_data.map((d,i) => {
+//     return { x: graph_1.x_scale(graph_1.x_data[i]), y: graph_1.y_scale(graph_1.y_data[i]) }
+//   })
+//   graph_1.path.attrs({ d: line( graph_1.path_data ) });
+// }
+
+/******************************************************************************/
+
 LogisiticModel.prototype.draw = function(){
 
   /* Chart 1 */
@@ -217,21 +333,24 @@ LogisiticModel.prototype.draw = function(){
   var trace3 = { x: [this.N0.value], y: [eval(this.exp)], mode: 'markers', name: 'Initial Population' };
 
   var data = [trace1, trace2_stable, trace2_unstable, trace3];
-  var layout = { title: 'Phase Plot', xaxis: { title: 'Population →' }, yaxis: { title: 'Rate of change of Population →', range: [-10, 10] } };
-  Plotly.newPlot('chart_1', data, layout);
+  // var layout = { title: 'Phase Plot', xaxis: { title: 'Population →' }, yaxis: { title: 'Rate of change of Population →', range: [-10, 10] } };
+  var layout = { title: 'Phase Plot', xaxis: { title: 'Population →' }, yaxis: { title: 'Rate of change of Population →' } };
+  Plotly.newPlot('chart_1', data, layout, { staticPlot: true });
 
   /* Chart 2 */
-  var trace4 = { x: this.t_list, y: this.N_list, type: 'line' }
-  var data = [trace4];
+  var trace4 = { x: this.t_list, y: this.N_list, type: 'line', name: 'Trajectory' }
+  var trace4_a = { x: [0], y: [this.N0.value], mode: 'markers', name: 'Initial Population' }
+  var data = [trace4, trace4_a];
   var layout = { title: 'Trajectory plot', xaxis: { title: 'time →' }, yaxis: { title: 'Population →', range: [this.N0.min, this.N0.max], zeroline: false } };
-  Plotly.newPlot('chart_2', data, layout);
+  Plotly.newPlot('chart_2', data, layout, { staticPlot: true });
 
   /* Chart 3 */
   var trace5 = { x: this.K_array, y: this.roots_array[1].map(d => { return d }), type: 'line' }
   var trace6 = { x: this.K_array, y: this.roots_array[0].map(d => { return d }), type: 'line' }
   var data = [trace5, trace6];
   var layout = { title: 'Nodes plot', xaxis: { title: 'K →' }, yaxis: { title: 'Roots →' } };
-  Plotly.newPlot('chart_3', data, layout);
+  Plotly.newPlot('chart_3', data, layout, { staticPlot: true });
+
 }
 
 /******************************************************************************/
